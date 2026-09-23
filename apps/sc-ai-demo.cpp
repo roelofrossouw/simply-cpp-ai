8987498d30c2de33ce93097517e5ed3936abdf44
Roelof Rossouw <roelof@1web.co.za>
2026-09-23 15:58:19 +0200

    Updating message

--- apps/sc-ai-demo.cpp
+++ apps/sc-ai-demo.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 int main(int argc, char **argv) {
     if (argc < 2) {
-        cerr << "Warning: no image filename was passed as a parameter." << endl;
+        cerr << "Please specify an image to run detection on." << endl;
         return 0;
     }
 
